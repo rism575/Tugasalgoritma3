7fd89cad250771596d9b51261e1225e011e1f2ee
rism575 <132285072+rism575@users.noreply.github.com>
2023-10-08 23:50:12 +0700

    Create Tugasalgoritma3.java

---
+++ Tugasalgoritma3.java
@@ -0,0 +1,6 @@
+public class pecahan {
+    public static void main(String[] args) throws Exception {
+        
+        int pecahan [] = [100000,50000,20000,10000,5000,2000,1000,500,100];
+        system.out.println(100000+50000+20000+10000+5000+1000+500+100 + "=" +196300);
+} 
